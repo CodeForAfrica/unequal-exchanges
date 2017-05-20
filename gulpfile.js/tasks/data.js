ec21b1b368defcbd645004e6295dc88d6ddeba15
Emma Lewis <emmarachellewis@gmail.com>
2017-05-20 14:25:00 +0100

    remove jsonfile

--- gulpfile.js/tasks/data.js
+++ gulpfile.js/tasks/data.js
@@ -1,10 +1,6 @@
-// var data = require('../../src/scripts/data/chord-transactions.json');
 var fs           = require("fs");
 var join         = require('path').join;
 var config       = require('../config');
-var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-// var jsonfile = require('jsonfile');
 
 module.exports = {
     sort: function() {
@@ -52,45 +48,9 @@ module.exports = {
                 }
             }
 
-                // if(typeof(countries[data[i]['sending_country']]) == 'undefined') {
-                //     countries.push({
-                //         'name': 
-                //     })
-                //     countries[data[i]['sending_country']] = new Array();
-                //     countries[data[i]['sending_country']].total = parseInt([data[i]['sending_total']], 10);
-                //     countries[data[i]['sending_country']].receiving_countries = new Array();
-                //     countries[data[i]['sending_country']]['receiving_countries'][data[i]['receiving_country']] = parseInt([data[i]['sending_total']], 10);
-                // } else {
-                //     countries[data[i]['sending_country']].total += parseInt([data[i]['sending_total']], 10);
-                //     countries[data[i]['sending_country']]['receiving_countries'][data[i]['receiving_country']] = parseInt([data[i]['sending_total']], 10);
-                // }
-
-            // console.log(countries)
-
-            console.log(JSON.stringify(countries));
             fs.writeFile(join(config.scripts.src, 'data/transactions.json'), JSON.stringify(countries), function(err) {
                 console.log(err);
             });
-            
-            // var file = join(config.scripts.src, 'data/transactions.json');
-            // jsonfile.writeFileSync(file, JSON.stringify(countries));
         });
-
-        // fs.readFile(join(config.scripts.src, 'data/transactions.json'), 'utf-8', function(err, data) {
-        //     if (err) throw err
-
-        //     var arrayOfObjects = JSON.parse(data)
-        //     arrayOfObjects.users.push({
-        //         name: "Mikhail",
-        //         age: 24
-        //     })
-
-        //     console.log(arrayOfObjects)
-
-        //     fs.writeFile(join(config.scripts.src, 'data/transactions.json'), JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
-        //         if (err) throw err
-        //         console.log('Done!')
-        //     })
-        // })
     }
 };
