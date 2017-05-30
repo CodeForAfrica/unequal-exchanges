712100a2daf2e1813bf19b881a53ad89b5aae87b
Emma Lewis <emmarachellewis@gmail.com>
2017-05-30 22:15:21 +0100

    sending done with resize

--- src/scripts/modules/transactions.js
+++ src/scripts/modules/transactions.js
@@ -1,11 +1,14 @@
-import $ from 'jquery'
-import sendingCountries from '../data/transactions.json'
-import * as d3 from 'd3'
-import {COLOR_SENDING} from './globals'
+import $                                from 'jquery'
+import sendingCountries                 from '../data/transactions.json'
+import * as d3                          from 'd3'
+import {COLOR_SENDING}                  from './globals'
+import throttle                         from '../utils/throttle.js'
 
 let MAX_TOTAL = 0
 const LINE_OPACITY = 0.2
 let COUNTRY_WIDTH = 0
+let COUNTRY_HEIGHT = 0
+let LINES_ARRAY = []
 
 class Transactions {
     constructor() {
@@ -18,11 +21,108 @@ class Transactions {
         this.$activeName = $('.transactions__active__name')
         this.$activeTotal = $('.transactions__active__total')
         this.$activeClose = $('.transactions__active__close')
+        this.$window = $(window)
     }
 
     init() {
         this.addCountries()
+        this.calculateSizes()
+        this.drawLines()
         this.mouseEvents()
+
+        throttle('resize', 'resize.transactions')
+        this.$window.on('resize.transactions', () => {
+            this.resize()
+        })
+    }
+
+    resize() {
+        COUNTRY_WIDTH = this.$countries.first().outerWidth()
+        COUNTRY_HEIGHT = this.$countries.first().outerHeight()
+
+        this.width = this.$container.outerWidth()
+        this.height = this.$container.outerHeight()
+        this.svg.attr('viewBox', `0 0 ${this.width} ${this.height}`)
+
+        const $activeCountry = $('.transactions__country.active')
+        let receivingIds = null
+        let receivingValues = null
+        if ($activeCountry.length > 0) {
+            receivingIds = $('.transactions__country.active').data('receiving').toString().split(',')
+            receivingValues = $('.transactions__country.active').data('receivingValues').toString().split(',')    
+        }
+        
+        $('.transactions__sending').each((index, element) => {
+            const $circle = $(element)
+            const $country = $circle.parents('.transactions__country')
+            if ($country.hasClass('receiving')) {
+                const rIndex = receivingIds.indexOf($country.data('id'))
+                const receivingWidth = COUNTRY_WIDTH * Math.sqrt(receivingValues[rIndex] / MAX_TOTAL)
+                $country.find('.transactions__sending').css({
+                    'width': receivingWidth,
+                    'height': receivingWidth
+                })
+            } else {
+                const sendingWidth = COUNTRY_WIDTH * Math.sqrt($country.data('total') / MAX_TOTAL)
+                $country.find('.transactions__sending').css({
+                    'width': sendingWidth, 
+                    'height': sendingWidth
+                })
+            }
+        })
+
+        d3.selectAll('.country__line')
+            .attr('x1', (d) => {
+                let $country = $(`#country-${d.sending}`)
+                return $country.offset().left + COUNTRY_WIDTH / 2
+            })
+            .attr('y1', (d) => {
+                let $country = $(`#country-${d.sending}`)
+                return $country.offset().top - this.$container.offset().top + 11 + COUNTRY_HEIGHT / 2
+            })
+            .attr('x2', (d) => {
+                return $(`#country-${d.receiving}`).offset().left + COUNTRY_WIDTH / 2
+            })
+            .attr('y2', (d) => {
+                return $(`#country-${d.receiving}`).offset().top - this.$container.offset().top + 11 + COUNTRY_HEIGHT / 2
+            })
+    }
+
+    drawLines() {
+        let lines = this.svg.selectAll('.country__line')
+            .data(LINES_ARRAY)
+
+        let enterSel = lines.enter()
+            .append('line')
+            .attr('x1', (d) => {
+                let $country = $(`#country-${d.sending}`)
+                return $country.offset().left + COUNTRY_WIDTH / 2
+            })
+            .attr('y1', (d) => {
+                let $country = $(`#country-${d.sending}`)
+                return $country.offset().top - this.$container.offset().top + 11 + COUNTRY_HEIGHT / 2
+            })
+            .attr('x2', (d) => {
+                return $(`#country-${d.receiving}`).offset().left + COUNTRY_WIDTH / 2
+            })
+            .attr('y2', (d) => {
+                return $(`#country-${d.receiving}`).offset().top - this.$container.offset().top + 11 + COUNTRY_HEIGHT / 2
+            })
+            .attr('stroke', COLOR_SENDING)
+            .attr('stroke-width', 1)
+            .attr('opacity', LINE_OPACITY)
+            .attr('class', 'country__line') 
+        lines.merge(enterSel)
+            .transition()
+            .attr('opacity', (d) => {
+                if (d.status === 'neutral') {
+                    return LINE_OPACITY
+                } else if (d.status === 'active') {
+                    return 1
+                } else {
+                    return 0
+                }
+            })
     }
 
     addCountries() {
@@ -54,13 +154,11 @@ class Transactions {
         })
 
         this.$countries = $('.transactions__country')
-
-        this.calculateSizes()
     }
 
     calculateSizes() {
         COUNTRY_WIDTH = this.$countries.first().outerWidth()
-        const linesList = new Array(sendingCountries.length)
+        COUNTRY_HEIGHT = this.$countries.first().outerHeight()
 
         $.each(sendingCountries, (index, country) => {
             const $country = $(`#country-${country.id}`)
@@ -69,37 +167,21 @@ class Transactions {
                 'width': sendingWidth, 
                 'height': sendingWidth
             })
-
-            const countryLeft = $country.offset().left + COUNTRY_WIDTH / 2
-            const countryTop = $country.offset().top - this.$container.offset().top + 11 + $country.outerHeight() / 2 
-    
+            
             $.each(country.receiving_countries, (i, receivingCountry) => {
-                const $receivingCountry = $(`#country-${receivingCountry.id}`)
-                if (linesList[country.id] === undefined && linesList[receivingCountry.id] === undefined || 
-                linesList[country.id] === undefined && linesList[country.id] !== undefined && !linesList[receivingCountry.id].includes(country.id) ||
-                linesList[country.id] !== undefined && !linesList[country.id].includes(receivingCountry.id) && linesList[receivingCountry.id] === undefined ||
-                linesList[country.id] !== undefined && linesList[receivingCountry.id] !== undefined && !linesList[country.id].includes(receivingCountry.id) && !linesList[receivingCountry.id].includes(country.id)) {
-                    let bilateral = false
-                    if(country.sending_countries.find((e) => { 
+                if (LINES_ARRAY.find((e) => {
+                    return e.sending === receivingCountry.id && e.receiving === country.id
+                }) === undefined) {
+                    let bilateral = $.grep(country.sending_countries, (e) => {
                         return e.id === receivingCountry.id 
-                    }) !== undefined) {
-                        bilateral = true
-                    }
-                    this.svg.append('line')
-                        .attr('x1', countryLeft)
-                        .attr('y1', countryTop)
-                        .attr('x2', $receivingCountry.offset().left + COUNTRY_WIDTH / 2)
-                        .attr('y2', $receivingCountry.offset().top - this.$container.offset().top + 11 + $country.outerHeight() / 2)
-                        .data([{'sending': country.id, 'receiving': receivingCountry.id, 'bilateral': bilateral}])
-                        .attr('stroke', COLOR_SENDING)
-                        .attr('stroke-width', 1)
-                        .attr('opacity', LINE_OPACITY)
-                        .attr('class', 'country__line')
-                    if (linesList[country.id] === undefined) {
-                        linesList[country.id] = new Array(sendingCountries.length)
-                    }
-                    linesList[country.id].push(receivingCountry.id)
-                } 
+                    }).length > 0
+                    LINES_ARRAY.push({
+                        'sending': country.id, 
+                        'receiving': receivingCountry.id, 
+                        'bilateral': bilateral, 
+                        'status': 'neutral'
+                    })
+                }
             })
         })
         this.height = this.$container.outerHeight()
@@ -129,6 +211,10 @@ class Transactions {
             this.$activeTotal.text($country.data('total'))
             this.$active.addClass('active')
             this.$countries.not(`#country-${id}`).addClass('hide')
+                .find('.transactions__sending').css({
+                    'width': 0,
+                    'height': 0
+                })
             const receivingIds = $(`#country-${id}`).data('receiving').toString().split(',')
             const receivingValues = $(`#country-${id}`).data('receivingValues').toString().split(',')
             
@@ -172,11 +258,13 @@ class Transactions {
                                 this.mouseEvents()
                             }
                         })
-                $.each(receivingIds, (index, countryId) => {
-                    const $receivingCountry = $(`#country-${countryId}`)
-                    const sendingWidth = COUNTRY_WIDTH * Math.sqrt($receivingCountry.data('total') / MAX_TOTAL)
-                    $receivingCountry.find('.transactions__sending').css({
-                        'width': sendingWidth,
+                // $.each(receivingIds, (index, countryId) => {
+                $('.transactions__sending').each((index, element) => {
+                    const $circle = $(element)
+                    const $country = $circle.parents('.transactions__country')
+                    const sendingWidth = COUNTRY_WIDTH * Math.sqrt($country.data('total') / MAX_TOTAL)
+                    $country.find('.transactions__sending').css({
+                        'width': sendingWidth, 
                         'height': sendingWidth
                     })
                 })
